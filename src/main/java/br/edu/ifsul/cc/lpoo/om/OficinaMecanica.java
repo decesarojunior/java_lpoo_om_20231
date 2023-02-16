a48c3b9733a07603f63c5c526ba163e8bca40ee7
decesarojunior <decesarojunior@yahoo.com.br>
2023-02-16 10:53:30 -0300

    projeto base para a disciplina de LPOO para o estudo de caso: oficina mecanica.

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/om/OficinaMecanica.java
@@ -0,0 +1,14 @@
+
+
+package br.edu.ifsul.cc.lpoo.om;
+
+/**
+ *
+ * @author telmo
+ */
+public class OficinaMecanica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
